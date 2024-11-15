8afc75e3fd2a988b1eabd66804997397b19cefac
ahn_h_j <tkgkd246@gmail.com>
2024-11-15 21:26:39 +0900

    [Java Intermediate] 예외 처리가 포함된 계산기 프로그램 작성하기

---
+++ Java/JavaExp/Java Intermediate Practice/ExceptionCalculate.java
@@ -0,0 +1,15 @@
+package java_intermediate;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ExceptionCalculate {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try{
+
+        }catch (Exception e){}
+    }
+}
