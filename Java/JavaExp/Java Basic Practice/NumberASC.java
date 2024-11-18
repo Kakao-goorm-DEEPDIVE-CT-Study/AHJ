0642d63242ffd6af6e6d45e96d55b725e25004ac
ahn_h_j <tkgkd246@gmail.com>
2024-11-18 10:08:23 +0900

    [Java Basic] (Java) 5개의 정수 오름차순 정렬하기 : 피드백 반영

--- Java/JavaExp/Java Basic Practice/NumberASC.java
+++ Java/JavaExp/Java Basic Practice/NumberASC.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class NumberASC {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       /* BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         bw.write("배열에 들어갈 숫자를 입력하세요 : ");
@@ -23,6 +23,40 @@ public class NumberASC {
         for(int num : inputNumber){
             bw.write(num + " ");
         }
+        bw.flush();*/
+
+        //피드백 후 개선 코드
+        //피드백 : 정렬 직접 구현해보기
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("배열에 들어갈 숫자를 입력하세요 : ");
+        bw.flush();
+        String[] input = br.readLine().split(" ");
+
+        int[] inputNumber = new int[input.length];
+        for(int i = 0; i < input.length; i++) {
+            inputNumber[i] = Integer.parseInt(input[i]);
+        }
+        arraySort(inputNumber);
+        bw.write("정렬된 배열 : ");
+        for(int num : inputNumber){
+            bw.write(num + " ");
+        }
         bw.flush();
     }
+    public static void arraySort(int[] inputNumber){
+        //삽입정렬
+        for(int outerIndex = 1; outerIndex < inputNumber.length; outerIndex++){
+            for(int innerIndex = outerIndex; innerIndex > 0; innerIndex--){
+                if(inputNumber[innerIndex] < inputNumber[innerIndex - 1]){
+                    int temp = inputNumber[innerIndex];
+                    inputNumber[innerIndex] = inputNumber[innerIndex - 1];
+                    inputNumber[innerIndex - 1] = temp;
+                }
+            }
+        }
+    }
+
 }
