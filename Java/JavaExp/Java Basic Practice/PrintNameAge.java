0bf91e0a33e805c6d64e2dc46e508425a589121c
ahn_h_j <tkgkd246@gmail.com>
2024-11-18 10:00:23 +0900

    [Java Basic] (Java) 자신의 이름과 나이를 출력하는 Java 프로그램 작성하기 : 피드백 반영

--- Java/JavaExp/Java Basic Practice/PrintNameAge.java
+++ Java/JavaExp/Java Basic Practice/PrintNameAge.java
@@ -6,10 +6,26 @@ import java.io.InputStreamReader;
 
 public class PrintNameAge {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("이름,나이 : ");
+        /*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("이름, 나이 : ");
         String[] input = br.readLine().split(" ");
         System.out.println(input[0] + " " + Integer.parseInt(input[1]));
-        
+        */
+
+        //피드백 후 개선 코드
+        //피드백 : 이름에 띄어쓰기가 있을 시 오류 발생
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("이름, 나이 : ");
+        //input 받기
+        String input = br.readLine();
+        //age부분의 index 추출
+        int index = input.lastIndexOf(" ");
+        //이름 추출
+        String name = input.substring(0,index);
+        //나이 추출
+        String age = input.substring(index);
+
+        System.out.println("이름 : " + name + " 나이 : " + age);
     }
 }
