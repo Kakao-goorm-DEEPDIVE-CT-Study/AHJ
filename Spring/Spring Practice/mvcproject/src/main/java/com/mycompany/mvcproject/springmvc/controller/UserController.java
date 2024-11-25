416a208b69dc8d179b95ef2b466359327e47dd73
ahn_h_j <tkgkd246@gmail.com>
2024-11-25 16:47:53 +0900

    [Spring Lecture] Spring Lecture Practice : Spring MVC

--- Spring/Spring Practice/mvcproject/src/main/java/com/mycompany/mvcproject/springmvc/controller/UserController.java
+++ Spring/Spring Practice/mvcproject/src/main/java/com/mycompany/mvcproject/springmvc/controller/UserController.java
@@ -1,30 +1,29 @@
-package com.mycompany.mvcproject.controller;
+package com.mycompany.mvcproject.springmvc.controller;
 
-import com.mycompany.mvcproject.domain.User;
+import com.mycompany.mvcproject.login.domain.User;
+import com.mycompany.mvcproject.springmvc.domain.UserEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/userMVC")
 public class UserController {
 
-    @GetMapping("/signup")
+    @GetMapping("/signupMVC")
     public String signup(Model model){
-        User user = new User();
-        model.addAttribute("user",user);
+        UserEntity userEntity = new UserEntity();
+        model.addAttribute("user",userEntity);
         return "signup";
     }
 
-    @PostMapping(value = "signup")
+    @PostMapping(value = "signupMVC")
 //    @ResponseBody
     //@ModelAttribute User user "user"와 user가 같을때는 가능
-    public String signup(@ModelAttribute("user") User user){//위와 같은 방식
+    public String signup(@ModelAttribute("user") UserEntity user){//위와 같은 방식
         //Model model은 다음페이지로 넘겨주기 위함
         //넘겨주려면 model.addAttribute로 데이터를 다시 담아줘야함
         System.out.println(user.toString());
         return "redirect:/hello";
     }
-
-
 }
