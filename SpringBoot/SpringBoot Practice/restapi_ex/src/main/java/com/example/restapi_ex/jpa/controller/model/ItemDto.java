7cbe081bb605a3a66719e115b535a8c9db666d35
ahn_h_j <tkgkd246@gmail.com>
2024-11-29 14:21:16 +0900

    [SpringBoot Lecture] SpringBoot Lecture Practice : JPA

---
+++ SpringBoot/SpringBoot Practice/restapi_ex/src/main/java/com/example/restapi_ex/jpa/controller/model/ItemDto.java
@@ -0,0 +1,16 @@
+package com.example.restapi_ex.jpa.controller.model;
+
+
+import lombok.*;
+
+//@Getter
+//@Setter
+//@ToString
+// ==
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemDto {
+    private String id;
+    private String name;
+}
