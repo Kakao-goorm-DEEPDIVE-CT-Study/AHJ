98d030d86eda2e12d7d5a2d45698a8500f9e4737
ahn_h_j <tkgkd246@gmail.com>
2024-11-29 14:02:44 +0900

    [SpringBoot Lecture] SpringBoot Lecture Practice : JPA

--- SpringBoot/SpringBoot Practice/restapi_ex/src/main/java/com/example/restapi_ex/controller/entity/ItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.restapi_ex.controller.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<ItemEntity, String> {
-}
