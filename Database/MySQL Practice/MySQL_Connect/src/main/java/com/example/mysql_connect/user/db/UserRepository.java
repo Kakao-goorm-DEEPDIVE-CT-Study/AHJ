bb87654eac162500152db8ef76a2e0580e6f9718
ahn_h_j <tkgkd246@gmail.com>
2025-01-06 23:15:22 +0900

    no message

--- Database/MySQL Practice/MySQL_Connect/src/main/java/com/example/mysql_connect/user/db/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.mysql_connect.user.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity,Long> {
-}
--- Database/MySQL Practice/myBoard/src/main/java/com/example/myboard/reply/db/ReplyRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.myboard.reply.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReplyRepository extends JpaRepository<ReplyEntity,Long> {
-}
